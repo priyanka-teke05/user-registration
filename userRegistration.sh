44254bccfeb22b82feb0968833c3e01e19c5e5f6
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-21 15:43:50 +0530

    ADDED: validation of first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,25 @@
 #!/bin/bash -x
 
+#used for an exterbnal globbing
+shopt -s extglob
+
 echo "Welcome to User Registration Problem"
+
+#Patterns
+firstNamePattern="^[A-Z]{1}[a-z]{2,}$"
+
+read -p "Enter First Name : " firstName
+
+#Validating user first name
+function validateUserFirstName()
+{
+	local userDetails=$1 pattern=$2
+	if [[ $userDetails =~ $pattern ]]
+	then
+		echo "Valid Name"
+	else
+		echo "Invalid name"
+	fi
+}
+
+validateUserFirstName $firstName $firstNamePattern
