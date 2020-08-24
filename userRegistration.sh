82a07900dd12e670a731ab22a321fc1d029b8d54
priyanka-teke05 <priyankateke1@gmail.com>
2020-08-24 14:34:00 +0530

    ADDED: validate all sample emails

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,6 +5,8 @@ shopt -s extglob
 
 echo "Welcome to User Registration Problem"
 
+declare -A emailSample
+
 #Patterns
 readonly NAME_PATTERN="^[[:upper:]]{1}[[:lower:]]{2,}$"
 readonly EMAIL_PATTERN="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
@@ -34,6 +36,40 @@ function validatePassword() {
 	fi
 }
 
+#validate all sample emails
+function validateAllEmailSample()
+{
+	#valid email id
+	emailSample[email-1]="abc@yahoo.com"
+	emailSample[email-2]="abc-100@yahoo.com"
+	emailSample[email-3]="abc.100@yahoo.com"
+	emailSample[email-4]="abc111@abc.com"
+	emailSample[email-5]="abc-100@abc.net"
+	emailSample[email-6]="abc.100@abc.com.au"
+	emailSample[email-7]="abc@1.com"
+	emailSample[email-8]="abc@gmail.com.com"
+
+	#invalid email id
+	emailSample[email-9]="abc-gmail.com"
+	emailSample[email-10]="abc@.com.my"
+	emailSample[email-11]="abc123@gmail.a"
+	emailSample[email-12]="abc123@.com"
+	emailSample[email-13]="abc123@.com.com"
+	emailSample[email-14]=".abc@abc.com"
+	emailSample[email-15]="abc()*@gmail.com"
+	emailSample[email-16]="abc@%*.com"
+	emailSample[email-17]="abc..2002@gmail.com"
+	emailSample[email-18]="abc.@gmail.com"
+	emailSample[email-19]="abc@abc@gmail.com"
+	emailSample[email-20]="abc@gmail.com.1a"
+	emailSample[email-21]="abc@gmail.com.aa.au"
+
+	for i in ${!emailSample[@]}
+	do
+		[[ ${emailSample[$i]} =~ $EMAIL_PATTERN ]] && echo "$i : ${emailSample[$i]} - Valid email" || echo "$i : ${emailSample[$i]} - Invalid email"
+	done
+}
+
 #read first name
 read -p "Enter First Name : " firstName
 validateUserDetails $firstName $NAME_PATTERN
@@ -53,3 +89,5 @@ validateUserMobileNumber
 #read password
 read -p "Enter Password(At least 8 characters long and atleast 1 uppercase letter 1 numeric and special characters)  : " password
 validatePassword $password
+
+validateAllEmailSample
